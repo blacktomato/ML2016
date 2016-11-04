3662fd58741ea0e816ddac9d9374e6ff542734c6
blacktomato <s90169@gmail.com>
2016-11-04 22:29:06 +0800

    temp

--- hw3_work/cnn.py
+++ hw3_work/cnn.py
@@ -4,10 +4,24 @@
  # File Name : cnn.py
  # Purpose : To classify the image
  # Creation Date : Fri 04 Nov 2016 10:41:51 AM CST
- # Last Modified : Fri 04 Nov 2016 10:43:58 AM CST
+ # Last Modified : Fri 04 Nov 2016 10:28:51 PM CST
  # Created By : SL Chung
 ##############################################################
-import numpy
+import numpy as np
 import pickle
 import sys
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+
+#load the label and unlabel data
+label_file = open(sys.argv[1] + 'all_label.p', "rb")
+unlabel_file = open(sys.argv[1] + 'all_unlabel.p', "rb")
+pre_label = pickle.load(label_file)
+pre_unlabel = pickle.load(unlabel_file)
+label_file.close()
+unlabel_file.close()
+
+label = np.asarray(pre_label)
+unlabel = np.asarray(pre_unlabel)
 
