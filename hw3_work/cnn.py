cb74cb3b742730277abbbbb0f6ce5cd3da5f5315
blacktomato <s90169@gmail.com>
2016-11-04 13:55:30 +0800

    Just start and not include the datas

---
+++ hw3_work/cnn.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# coding=utf-8
+##############################################################
+ # File Name : cnn.py
+ # Purpose : To classify the image
+ # Creation Date : Fri 04 Nov 2016 10:41:51 AM CST
+ # Last Modified : Fri 04 Nov 2016 10:43:58 AM CST
+ # Created By : SL Chung
+##############################################################
+import numpy
+import pickle
+import sys
+
