dce1cf04d3b2ee020b8b209e10d3e32462232706
SL Chung <s90169@gmail.com>
2016-11-14 08:39:35 +0800

    working on semi-supervise

--- hw3_work/cnn_fast.py
+++ hw3_work/cnn_fast.py
@@ -1,58 +1,102 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding=utf-8
 ##############################################################
  # File Name : cnn_fast.py
  # Purpose :
  # Creation Date : Fri 11 Nov 2016 04:50:40 PM CST
- # Last Modified : Sat 12 Nov 2016 05:44:44 PM CST
+ # Last Modified : Mon 14 Nov 2016 08:38:56 CST
  # Created By : SL Chung
 ##############################################################
 import numpy as np
 import pickle
 import sys
+
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers import Convolution2D, MaxPooling2D, Flatten
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 
+def train_cifar10(X, Y):
+    model = Sequential()
+    #Convolution
+    model.add(Convolution2D(30, 5, 5, border_mode='same', input_shape=(3, 32, 32)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((2,2)))
+
+    model.add(Convolution2D(60, 4, 4))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D((3,3)))
+    model.add(Dropout(0.1))
+
+    model.add(Flatten())
+    #Fully connected
+    model.add(Dense(100))
+    model.add(Activation('sigmoid'))
+    model.add(Dropout(0.25))
+    model.add(Dense(10))
+    model.add(Activation('softmax'))
+
+    model.summary()
+
+    #Training
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.fit(X, Y,validation_split=0.1, batch_size=300, nb_epoch=100)
+    #model.fit(xtrain[train], ytrain[train], batch_size=300, nb_epoch=100)
+    #scores = model.evaluate(xtrain[test], ytrain[test], verbose=0)
+    #print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+    #cvscores.append(scores[1] * 100)
+
+    #Validation
+    #print("%.2f%% (+/- %.2f%%)" % (np.mean(cvscores), np.std(cvscores)))
+    return model
 
 label = np.load("./Label.npy")      #10-500-3072
 unlabel = np.load("./Unlabel.npy")  #45000-3072
+test = np.load("./Test.npy")        #10000-3072
 
 label = label.reshape((5000, 3072))
-xtrain = label.reshape((5000, 3, 32, 32))
-ytrain = np.array([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-                   [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]]*500)
-ytrain = np.transpose(ytrain.reshape((500,10,10)), (2,0,1)).reshape(5000,10)
+unlabel = unlabel.reshape((45000, 3, 32, 32)) / 255.
+xtrain = label.reshape((5000, 3, 32, 32))/ 255.
+xtest = test.reshape((10000, 3, 32, 32)) / 255.
+temp = np.array([np.identity(10, dtype=int)]*500)
+ytrain = np.transpose(temp, (2,0,1)).reshape(5000,10)
+
+#shuffle for validation
+xtrain, ytrain = shuffle(xtrain, ytrain, random_state = 0)
+#fix random seed for reproducibility
+#seed = 7
+#np.random.seed(seed)
+#define 10-fold cross validation test harness
+#kfold = KFold(n_splits=10)
+#cvscores=[]
 
 #Start training
-#Convolution
-model = Sequential()
-model.add(Convolution2D(32, 3, 3, border_mode='same', input_shape=(3, 32, 32)))
-model.add(MaxPooling2D((2,2)))
-model.add(Convolution2D(50,3,3))
-model.add(MaxPooling2D((2,2)))
-model.add(Flatten())
-#Fully connected
-model.add(Dense(100))
-model.add(Activation('sigmoid'))
-model.add(Dense(10))
-model.add(Activation('softmax'))
+model = train_cifar10(xtrain, ytrain)
+#for train, test in kfold.split(xtrain, ytrain):
 
-model.summary()
+unlabel_result = model.predict(unlabel, batch_size=32, verbose=0)
+for i in range(len(unlabel_result)):
+    if unlabel_result[i].max() > unlabel_result[i].sum() / 2:
+        print('Adding unlabel data [', str(i), ']...')
+        xtrain = np.vstack(( xtrain, unlabel[i].reshape((1, 3, 32, 32)) ))
+        ytrain = np.vstack(( ytrain, ((unlabel_result[i] == unlabel_result[i].max)*1).reshape(1, 10) ))
 
-#Training
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(xtrain, ytrain, batch_size=200, nb_epoch=1)
+print(xtrain.shape)
 
-#Validation
-result = model.evaluate(xtrain,ytrain,batch_size=10000)
-print(result[1])
+model = train_cifar10(xtrain, ytrain)
+
+test_result = model.predict(xtest, batch_size=32, verbose=0)
+#output file
+output = open(sys.argv[1], "w+")
+output.write("ID,class\n")
+
+for i in range(len(test_result)):
+    print(test_result[i])
+    line = str(i) + "," + str(int(np.argmax(test_result[i]))) + "\n"
+output.close()
+
+print("Output file:", sys.argv[1]) 
+
--- hw3_work/data_process.py
+++ hw3_work/data_process.py
@@ -4,7 +4,7 @@
  # File Name : cnn.py
  # Purpose : To classify the image
  # Creation Date : Fri 04 Nov 2016 10:41:51 AM CST
- # Last Modified : Fri 11 Nov 2016 04:49:37 PM CST
+ # Last Modified : Sun 13 Nov 2016 00:35:44 CST
  # Created By : SL Chung
 ##############################################################
 import numpy as np
@@ -16,15 +16,18 @@ from keras.layers import Dense, Activation
 #load the label and unlabel data
 label_file = open(sys.argv[1] + 'all_label.p', "rb")
 unlabel_file = open(sys.argv[1] + 'all_unlabel.p', "rb")
+test_file = open(sys.argv[1] + 'test.p', "rb")
 pre_label = pickle.load(label_file)
 pre_unlabel = pickle.load(unlabel_file)
+pre_test = pickle.load(test_file)
 label_file.close()
 unlabel_file.close()
+test_file.close()
 
-label = np.asarray(pre_label)      #10-500-3072
-unlabel = np.asarray(pre_unlabel)  #45000-3072
+label = np.asarray(pre_label)           #10-500-3072
+unlabel = np.asarray(pre_unlabel)       #45000-3072
+test = np.asarray(pre_test['data'])     #10000
 
 np.save("./Label", label)
 np.save("./Unlabel", unlabel)
-
-
+np.save("./Test", test)
