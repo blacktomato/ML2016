c1045a33ec3ebf08fd32cb155e4b084200c25028
SL Chung <s90169@gmail.com>
2016-11-18 17:25:37 +0800

    change for auto

--- hw3/train.sh
+++ hw3/train.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 ./data_process.py $1
-KERAS_BACKEND=theano THEANO_FLAGS='floatX=float32,device=gpu,lib.cnmem=0.6,mode=FAST_RUN' python3 semi-super_fast.py $2
+KERAS_BACKEND=theano THEANO_FLAGS='floatX=float32,device=gpu,lib.cnmem=0.6,mode=FAST_RUN' python3 semi-auto.py $2 $3
