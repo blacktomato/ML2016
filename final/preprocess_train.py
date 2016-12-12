25ae53b5be77979cf3082992ac30e7b674b35685
b02901071 <b02901071@ntu.edu.tw>
2016-12-12 20:56:37 +0800

    1/20

--- final/preprocess_train.py
+++ final/preprocess_train.py
@@ -17,14 +17,10 @@ from numpy import genfromtxt
 is_train = np.arange(33749186)
 shuffle(is_train, random_state = 0)
 
-# train_data = np.zeros((69713385, 799))
-# train_ans  = np.zeros((69713385, 2))
-# valid_data = np.zeros((17428346, 799))
-# valid_ans  = np.zeros((17428346, 2))
-train_data = np.zeros((26999349, 799))
-train_ans  = np.zeros((26999349, 2)).astype('int64')
-valid_data = np.zeros((6749837, 799))
-valid_ans  = np.zeros((6749837, 2)).astype('int64')
+# train_data = np.zeros((26999349, 799))
+# train_ans  = np.zeros((26999349, 2)).astype('int64')
+# valid_data = np.zeros((6749837, 799))
+# valid_ans  = np.zeros((6749837, 2)).astype('int64')
 
 #reading Event    [23120127 :   4]
 #reading Document [ 3000000 : 397]
@@ -37,7 +33,7 @@ click_data = genfromtxt(sys.argv[2] + '/clicks_train_small.csv',
                          delimiter=',', dtype='int64', skip_header=1)
 
 #train_data        
-click_train = click_data[is_train[:3374918]]
+click_train = click_data[is_train[:1687459]]
 event = Event[click_train[:, 0]]
 ad = Ad[click_train[:, 1]]
 display = np.hstack((Document[event[:, 0]], event[:, 1:]))
@@ -45,8 +41,8 @@ ad      = np.hstack((Document[   ad[:, 0]],    ad[:, 1:]))
 data    = np.hstack((display, ad))
 ans     = np.vstack((click_train[:, 2], 1 - click_train[:, 2])).T 
 
-train_data = data[is_train[:2699934]]
-train_ans  =  ans[is_train[:2699934]]
+train_data = data[is_train[:1349967]]
+train_ans  =  ans[is_train[:1349967]]
             
-valid_data = data[is_train[2699934:3374918]]
-valid_ans  =  ans[is_train[2699934:3374918]]
+valid_data = data[is_train[1349967:1687459]]
+valid_ans  =  ans[is_train[1349967:1687459]]
