506d3b7a81c9b342a7dac0fd96a4b7ba26956680
blacktomato <s90169@gmail.com>
2016-12-28 16:14:41 +0800

    littel

--- final/easy_connect.py
+++ final/easy_connect.py
@@ -4,7 +4,7 @@
  # File Name : easy_connect.py
  # Purpose :
  # Creation Date : Tue 27 Dec 2016 11:18:54 PM CST
- # Last Modified : Tue 27 Dec 2016 11:43:37 PM CST
+ # Last Modified : Wed 28 Dec 2016 12:57:50 AM CST
  # Created By : SL Chung
 ##############################################################
 import pandas as pd
@@ -15,11 +15,11 @@ a = pd.read_csv("./ad.csv")
 
 train = pd.merge(train, d, on="display_id")
 train = train.drop("display_id", 1)
-train["temp"] = train.clicked.astype(str).str.cat(train.d.adtype(str),sep=" ")
+train["temp"] = train.clicked.astype(str).str.cat(train.d.astype(str),sep=" ")
 train = train.drop(["clicked","d"], 1)
 train=pd.merge(train, a, on="ad_id")
 train = train.drop("ad_id", 1)
-train["i"] = train.temp.astype(str).str.cat(train.a.adtype(str))
+train["i"] = train.temp.astype(str).str.cat(train.a.astype(str))
 train = train.drop(["temp","a"], 1)
 
 train = train.sort_values(["display_id","ad_id"])
@@ -34,7 +34,7 @@ test = pd.merge(test, d, on="display_id")
 del d
 test=pd.merge(test, a, on="ad_id")
 del a
-test["i"] = test.d.astype(str).str.cat(test.a.adtype(str))
+test["i"] = test.d.astype(str).str.cat(test.a.astype(str))
 test = test.drop(["d","a"], 1)
 
 test = test.sort_values(["display_id","ad_id"])
